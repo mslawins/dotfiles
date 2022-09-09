8e75ec76808cf515036b5809eb5d34f6a6378b1c
Marek Sławiński <marek.slawinski@live.com>
2022-09-09 13:45:24 +0200

    Add typescript snippet

--- nvim/.config/nvim/lua/mslawins/snippets/typescript.lua
+++ nvim/.config/nvim/lua/mslawins/snippets/typescript.lua
@@ -1,6 +1,6 @@
 local luasnip = require('luasnip')
-
 local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
 
 local s = luasnip.s
 local i = luasnip.insert_node
@@ -9,12 +9,25 @@ local it = s({ trig = 'it' }, fmt([[
     it('{}', () => {{ 
       {} 
     }}); 
-  ]], { i(1, 'test_name'), i(0) }))
+]], { i(1, 'test_name'), i(0) }))
 
 local describe = s('describe', fmt([[
     describe('{}', () => {{ 
       {}
     }});
-  ]], { i(1, 'testcase'), i(0) }))
+]], { i(1, 'testcase'), i(0) }))
 
-return { it, describe }
+local component = s({ trig = 'component' }, fmt([[
+    import {{ ChangeDetectionStrategy, Component }} from '@angular/core';
+
+    @Component({{
+      selector: '{}',
+      templateUrl: '{}.component.html',
+      changeDetection: ChangeDetectionStrategy.OnPush,
+    }})
+    export class {}Component {{
+      {}
+    }}
+]], { i(1), rep(1), i(2), i(0) }))
+
+return { it, describe, component }
