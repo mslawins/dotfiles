f59dbd429ace5b0c73c93a65a5b420e325e51e3e
Marek Sławiński <marek.slawinski@live.com>
2022-12-12 21:20:17 +0100

    Update colorscheme

--- nvim/.config/nvim/lua/mslawins/colorscheme.lua
+++ nvim/.config/nvim/lua/mslawins/colorscheme.lua
@@ -1,11 +1,11 @@
 -- for 'ellisonleao/gruvbox.nvim'
-require('gruvbox').setup({ contrast = 'hard', italic = false })
-vim.cmd([[colorscheme gruvbox]])
+-- require('gruvbox').setup({ contrast = 'hard', italic = false })
+-- vim.cmd([[colorscheme gruvbox]])
 
 -- for 'luisiacc/gruvbox-baby'
--- vim.g.gruvbox_baby_background_color = "dark"
--- vim.cmd[[colorscheme gruvbox-baby]]
+vim.g.gruvbox_baby_background_color = "dark"
+vim.cmd [[colorscheme gruvbox-baby]]
 
 -- for 'sainnhe/gruvbox-material'
 -- vim.g.gruvbox_material_background = "hard"
--- vim.cmd[[colorscheme gruvbox-material]]
+-- vim.cmd [[colorscheme gruvbox-material]]
