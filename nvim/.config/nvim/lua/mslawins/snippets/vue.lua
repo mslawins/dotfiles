6861ca7cf0bf946b4aff9f949b10b137fc0e8404
Marek Sławiński <marek.slawinski@live.com>
2023-01-30 13:46:24 +0100

    Update snippets

--- nvim/.config/nvim/lua/mslawins/snippets/vue.lua
+++ nvim/.config/nvim/lua/mslawins/snippets/vue.lua
@@ -1,7 +1,9 @@
 local luasnip = require('luasnip')
 local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
 
 local s = luasnip.s
+local i = luasnip.insert_node
 
 local component = s({ trig = '-v-component' }, fmt([[
   <script setup lang="ts">
@@ -14,4 +16,12 @@ local component = s({ trig = '-v-component' }, fmt([[
   </style>
 ]], {}))
 
-return { component }
+local define_emits = s({ trig = '-v-define-emits'}, fmt([[
+  interface {} {{
+    (e: '{}',): void;
+  }}
+
+  const emit = defineEmits<{}>();
+]], {i(1), i(2), rep(1)}))
+
+return { component, define_emits }
