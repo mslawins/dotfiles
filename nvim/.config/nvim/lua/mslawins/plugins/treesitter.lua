4e655feefb1aac3966d291f48dada5981695d485
Marek Sławiński <marek.slawinski@live.com>
2023-02-25 16:33:39 +0100

    Add SQL to treesitter

--- nvim/.config/nvim/lua/mslawins/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mslawins/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'typescript', 'javascript', 'rust', 'lua', 'html', 'scss', 'css', 'vue', 'markdown' },
+  ensure_installed = { 'typescript', 'javascript', 'rust', 'lua', 'html', 'scss', 'css', 'vue', 'markdown', 'sql' },
   sync_install = false,
   auto_install = true,
   highlight = { enable = true },
