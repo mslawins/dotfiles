5366413d86b0c5aff5b786bf28b990d341ceba94
Marek Sławiński <marek.slawinski@live.com>
2023-04-04 21:15:47 +0200

    Add *.tsx to vim_matchtag_files

--- nvim/.config/nvim/lua/mslawins/plugins/vim-matchtag.lua
+++ nvim/.config/nvim/lua/mslawins/plugins/vim-matchtag.lua
@@ -1,4 +1,4 @@
-vim.g.vim_matchtag_files = '*.html,*.vue'
+vim.g.vim_matchtag_files = '*.html,*.vue,*.tsx'
 vim.g.vim_matchtag_highlight_cursor_on = 1
 
 vim.api.nvim_set_hl(0, 'matchTag', { bg = '#d5c4a1', fg = '#1d2021' })
