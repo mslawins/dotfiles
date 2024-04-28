a651df9d04cd3ca626bccc456c6946b09d5067ff
Marek Sławiński <marek.slawinski@live.com>
2024-04-28 20:04:38 +0200

    Add comment about broken Volar >2.0

--- nvim/.config/nvim/lua/mslawins/plugins/mason.lua
+++ nvim/.config/nvim/lua/mslawins/plugins/mason.lua
@@ -1,1 +1,5 @@
+-- NOTE: Last working version of Volar is vue-language-server@1.8.27
+-- To install specific version: MasonInstall vue-language-server@x.y.z
+-- Versions >2.0 do not work with current LSP settings.
+-- Further investigation required to fix this problem.
 require('mason').setup()
