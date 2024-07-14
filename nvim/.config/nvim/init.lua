b635845073a6a363676243ab1181cb7e1525adbc
Marek Sławiński <marek.slawinski@live.com>
2024-07-14 17:35:18 +0200

    Switch off match tag for .vue files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,29 +1,29 @@
-require 'mslawins.options'
-require 'mslawins.keymaps'
-require 'mslawins.lazy'
-require 'mslawins.colorscheme'
+require("mslawins.options")
+require("mslawins.keymaps")
+require("mslawins.lazy")
+require("mslawins.colorscheme")
 
-require 'mslawins.plugins.barbar'
-require 'mslawins.plugins.ChatGPT'
-require 'mslawins.plugins.conform_nvim'
-require 'mslawins.plugins.gitblame'
-require 'mslawins.plugins.glow'
-require 'mslawins.plugins.gx'
-require 'mslawins.plugins.hardtime'
-require 'mslawins.plugins.harpoon'
-require 'mslawins.plugins.lspsaga'
-require 'mslawins.plugins.lualine'
-require 'mslawins.plugins.mason'
-require 'mslawins.plugins.nvim-autopairs'
-require 'mslawins.plugins.nvim-colorizer'
-require 'mslawins.plugins.nvim-lint'
-require 'mslawins.plugins.nvim-tree'
-require 'mslawins.plugins.nvim_comment'
-require 'mslawins.plugins.precognition'
-require 'mslawins.plugins.telescope'
-require 'mslawins.plugins.todo-comments'
-require 'mslawins.plugins.treesitter'
-require 'mslawins.plugins.vim-matchtag'
-require 'mslawins.plugins.vim-rooter'
+require("mslawins.plugins.barbar")
+require("mslawins.plugins.ChatGPT")
+require("mslawins.plugins.conform_nvim")
+require("mslawins.plugins.gitblame")
+require("mslawins.plugins.glow")
+require("mslawins.plugins.gx")
+require("mslawins.plugins.hardtime")
+require("mslawins.plugins.harpoon")
+require("mslawins.plugins.lspsaga")
+require("mslawins.plugins.lualine")
+require("mslawins.plugins.mason")
+require("mslawins.plugins.nvim-autopairs")
+require("mslawins.plugins.nvim-colorizer")
+require("mslawins.plugins.nvim-lint")
+require("mslawins.plugins.nvim-tree")
+require("mslawins.plugins.nvim_comment")
+require("mslawins.plugins.precognition")
+require("mslawins.plugins.telescope")
+require("mslawins.plugins.todo-comments")
+require("mslawins.plugins.treesitter")
+require("mslawins.plugins.vim-matchtag")
+require("mslawins.plugins.vim-rooter")
 
-require 'mslawins.cmp'
+require("mslawins.cmp")
--- nvim/.config/nvim/lua/mslawins/plugins/vim-matchtag.lua
+++ nvim/.config/nvim/lua/mslawins/plugins/vim-matchtag.lua
@@ -1,5 +1,6 @@
-vim.g.vim_matchtag_files = '*.html,*.vue,*.tsx'
+-- NOTE: Enabling this plugin for .vue files causes error, when opening nvim-tree
+vim.g.vim_matchtag_files = "*.html,*.tsx"
 vim.g.vim_matchtag_highlight_cursor_on = 1
 
-vim.api.nvim_set_hl(0, 'matchTag', { bg = '#d5c4a1', fg = '#1d2021' })
-vim.api.nvim_set_hl(0, 'matchTagError', { bg = '#d5c4a1', fg = '#cc241d' })
+vim.api.nvim_set_hl(0, "matchTag", { bg = "#d5c4a1", fg = "#1d2021" })
+vim.api.nvim_set_hl(0, "matchTagError", { bg = "#d5c4a1", fg = "#cc241d" })
